cdf0e6a2fdbccf6d165db4230698796de7a13dfa
Antonio Mancera Ubaldo Jr. <142708326+JunSayke@users.noreply.github.com>
2024-12-11 14:43:00 +0800

    [Hunyo] Add smooth scrolling and fade-in animation to layout
    Enabled smooth scrolling globally on the HTML element and added a fade-in animation to the body element. Updated Tailwind configuration and CSS to support custom animations and ensure consistent styling across elements. Removed redundant height utility classes for cleaner CSS structure.

--- theme/static_src/tailwind.config.js
+++ theme/static_src/tailwind.config.js
@@ -65,6 +65,15 @@ module.exports = {
             maxWidth: {
                 'minmax': '1240px'
             },
+            keyframes: {
+                fadeIn: {
+                  '0%': { opacity: 0 },
+                  '100%': { opacity: 1 },
+                },
+            },
+            animation: {
+                fadeIn: 'fadeIn 1s ease-in-out',
+            },
         }
     },
     plugins: [
